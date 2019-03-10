b1a1d0656b5335ea52f9bf4e34ffc263cd2e4193
monikabiskup <monika.biskup@onet.pl>
2019-03-10 20:29:54 +0100

    Create Spring Boot project

---
+++ src/main/java/com/topairplanes/springboot/topairplanes/TopairplanesApplication.java
@@ -0,0 +1,13 @@
+package com.topairplanes.springboot.topairplanes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopairplanesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopairplanesApplication.class, args);
+	}
+
+}
